8acc2d5ec06be985560c07648d0e3712e768df65
Divyanshu Bhargava <divyanshub024@gmail.com>
2023-01-18 15:05:56 +0400

    Use WidgetType in MiraiTextFieldParser

--- lib/src/widgets/mirai_text_field/mirai_text_field_parser.dart
+++ lib/src/widgets/mirai_text_field/mirai_text_field_parser.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mirai/mirai.dart';
+import 'package:mirai/src/utils/widget_type.dart';
 import 'package:mirai/src/widgets/mirai_text_field/mirai_text_field.dart';
 
 class MiraiTextFieldParser extends MiraiParser<MiraiTextField> {
-  // MiraiTextFieldParser({
-  //   MiraiTextField? model,
-  // }) : super(model);
-
   @override
   MiraiTextField getModel(Map<String, dynamic> json) =>
       MiraiTextField.fromJson(json);
 
   @override
-  String get type => 'textfield';
+  String get type => WidgetType.textField.name;
 
   @override
   Widget parse(BuildContext context, MiraiTextField model) {
