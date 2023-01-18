bb9b15a42c750849e5de340d07538bc3e9106c63
Divyanshu Bhargava <divyanshub024@gmail.com>
2023-01-18 12:50:49 +0400

    Map MiraiFontWeight to FontWeight

--- lib/src/utils/text_util.dart
+++ lib/src/utils/text_util.dart
@@ -1,4 +1,4 @@
-class TextUtil {}
+import 'dart:ui';
 
 enum MiraiFontWeight {
   w100,
@@ -9,5 +9,38 @@ enum MiraiFontWeight {
   w600,
   w700,
   w800,
-  w900,
+  w900;
+
+  FontWeight get value {
+    switch (this) {
+      case MiraiFontWeight.w100:
+        return FontWeight.w100;
+
+      case MiraiFontWeight.w200:
+        return FontWeight.w200;
+
+      case MiraiFontWeight.w300:
+        return FontWeight.w300;
+
+      case MiraiFontWeight.w400:
+        return FontWeight.w400;
+
+      case MiraiFontWeight.w500:
+        return FontWeight.w500;
+
+      case MiraiFontWeight.w600:
+        return FontWeight.w600;
+
+      case MiraiFontWeight.w700:
+        return FontWeight.w700;
+
+      case MiraiFontWeight.w800:
+        return FontWeight.w800;
+
+      case MiraiFontWeight.w900:
+        return FontWeight.w900;
+      default:
+        return FontWeight.normal;
+    }
+  }
 }
