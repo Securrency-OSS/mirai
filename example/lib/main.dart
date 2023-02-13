9701ddcec87a30fbcc9ba4e2eae957e703c7b3d7
Aasim Khan <iamasimkhanjadoon@gmail.com>
2023-02-13 16:26:55 +0400

    List of widgets in home screen are added.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,3 +22,30 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// 1. Mirai Appbar
+// 2. Mirai Bottom Navigation Bar 
+// 3. Mirai Card 
+// 4. Mirai Column 
+// 5. Mirai Default Tab Controller 
+// 6. Mirai Elevated Buttton 
+// 7. Mirai Floating Action Button 
+// 8. Mirai Icon 
+// 9. Mirai Icon Button 
+// 10. Mirai Image
+// 11. Mirai List Tile 
+// 12. Mirai List View 
+// 13. Mirai Outlined Button 
+// 14. Mirai Padding 
+// 15. Mirai Padding 
+// 16. Mirai Row 
+// 17. Mirai Scaffold 
+// 18. Mirai Scroll View 
+// 19. Mirai Sized Box 
+// 20. Mirai Tabbar 
+// 21. Mirai Tabbar View 
+// 22. Mirai Text 
+// 23. Mirai Text Button 
+// 24. Mirai Text Field 
+// 25. Mirai Text Form Field 
