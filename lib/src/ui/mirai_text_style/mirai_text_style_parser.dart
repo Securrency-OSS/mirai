dd191d5155feea42b701022be2c0b746590433ca
Divyanshu Bhargava <divyanshub024@gmail.com>
2023-02-14 15:30:46 +0400

    return null if MiraiTextStyle is empty

--- lib/src/ui/mirai_text_style/mirai_text_style_parser.dart
+++ lib/src/ui/mirai_text_style/mirai_text_style_parser.dart
@@ -3,20 +3,21 @@ import 'package:mirai/src/ui/mirai_text_style/mirai_text_style.dart';
 import 'package:mirai/src/utils/color_utils.dart';
 
 class MiraiTextStyleParser {
-  static TextStyle parse(MiraiTextStyle? textStyle) {
-    if (textStyle == null) return const TextStyle();
-
-    return TextStyle(
-      inherit: textStyle.inherit,
-      color: textStyle.color.toColor,
-      backgroundColor: textStyle.backgroundColor.toColor,
-      fontSize: textStyle.fontSize,
-      fontWeight: textStyle.fontWeight?.value,
-      fontStyle: textStyle.fontStyle,
-      letterSpacing: textStyle.letterSpacing,
-      wordSpacing: textStyle.wordSpacing,
-      textBaseline: textStyle.textBaseline,
-      height: textStyle.height,
-    );
+  static TextStyle? parse(MiraiTextStyle? textStyle) {
+    if (textStyle != null) {
+      return TextStyle(
+        inherit: textStyle.inherit,
+        color: textStyle.color.toColor,
+        backgroundColor: textStyle.backgroundColor.toColor,
+        fontSize: textStyle.fontSize,
+        fontWeight: textStyle.fontWeight?.value,
+        fontStyle: textStyle.fontStyle,
+        letterSpacing: textStyle.letterSpacing,
+        wordSpacing: textStyle.wordSpacing,
+        textBaseline: textStyle.textBaseline,
+        height: textStyle.height,
+      );
+    }
+    return null;
   }
 }
