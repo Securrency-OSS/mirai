2f696b8462152ff5a3043626abe30a1dca53352f
Aasim Khan <iamasimkhanjadoon@gmail.com>
2023-02-27 13:16:45 +0400

    Added border and shadow to the example display screen

--- example/lib/app/example/example_screen_parser.dart
+++ example/lib/app/example/example_screen_parser.dart
@@ -19,18 +19,34 @@ class ExampleScreenParser extends MiraiParser<ExampleScreen> {
         backgroundColor: const Color(0XFFf5f5f5),
         elevation: 0,
         leading: IconButton(
-            onPressed: () => Navigator.of(context).pop(),
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Color(0XFF3c3c3c),
-            )),
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color(0XFF3c3c3c),
+          ),
+        ),
       ),
       backgroundColor: const Color(0XFFf5f5f5),
       body: FutureBuilder(
         future: Mirai.fromAssets(model.assetPath, context),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return Padding(
-            padding: const EdgeInsets.all(24),
+          return Container(
+            margin: const EdgeInsets.fromLTRB(24, 16, 24, 24),
+            clipBehavior: Clip.hardEdge,
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.all(Radius.circular(12)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
             child: snapshot.data ??
                 const Center(
                   child: CircularProgressIndicator(),
