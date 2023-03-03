3fccdfb47a30a5d880d1023e5a26dd12e8fe98cf
Divyanshu Bhargava <divyanshub024@gmail.com>
2023-03-03 16:32:42 +0400

    Remove unwanted const files

--- examples/mirai_gallery/lib/values/constants.dart
+++
@@ -1,3 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const MAIN_SCREEN = "assets/json/main_screen.json";
--- examples/mirai_gallery/lib/values/strings.dart
+++
@@ -1,5 +0,0 @@
-class Strings {
-  static String appTitle = 'Mirai Demo';
-  static String textTitle = 'Mirai Text';
-  static String textFieldTitle = 'Mirai TextField';
-}
