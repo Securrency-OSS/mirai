204a7bc775562d3dfce2937a9f422c1dcb2ab119
Aasim Khan <iamasimkhanjadoon@gmail.com>
2023-03-24 14:52:39 +0400

    Added box-fit support for the Mirai image.

--- packages/mirai/lib/src/parsers/mirai_image/mirai_image.g.dart
+++ packages/mirai/lib/src/parsers/mirai_image/mirai_image.g.dart
@@ -18,6 +18,7 @@ _$_MiraiImage _$$_MiraiImageFromJson(Map<String, dynamic> json) =>
       color: json['color'] as String?,
       width: (json['width'] as num?)?.toDouble(),
       height: (json['height'] as num?)?.toDouble(),
+      fit: $enumDecodeNullable(_$BoxFitEnumMap, json['fit']),
     );
 
 Map<String, dynamic> _$$_MiraiImageToJson(_$_MiraiImage instance) =>
@@ -28,6 +29,7 @@ Map<String, dynamic> _$$_MiraiImageToJson(_$_MiraiImage instance) =>
       'color': instance.color,
       'width': instance.width,
       'height': instance.height,
+      'fit': _$BoxFitEnumMap[instance.fit],
     };
 
 const _$MiraiAlignmentEnumMap = {
@@ -46,4 +48,14 @@ const _$MiraiImageTypeEnumMap = {
   MiraiImageType.file: 'file',
   MiraiImageType.network: 'network',
   MiraiImageType.asset: 'asset',
+};
+
+const _$BoxFitEnumMap = {
+  BoxFit.fill: 'fill',
+  BoxFit.contain: 'contain',
+  BoxFit.cover: 'cover',
+  BoxFit.fitWidth: 'fitWidth',
+  BoxFit.fitHeight: 'fitHeight',
+  BoxFit.none: 'none',
+  BoxFit.scaleDown: 'scaleDown',
 };
