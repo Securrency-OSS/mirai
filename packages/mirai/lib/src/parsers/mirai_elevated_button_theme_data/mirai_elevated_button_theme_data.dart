9e3441105cf81c1e8962a4eaa07b14a5f40d56f9
Aasim Khan <iamasimkhanjadoon@gmail.com>
2023-04-04 10:47:41 +0400

    Refactored mirai button style parser and moved all type of button styles parsers into the button-style

--- packages/mirai/lib/src/parsers/mirai_elevated_button_theme_data/mirai_elevated_button_theme_data.dart
+++ packages/mirai/lib/src/parsers/mirai_elevated_button_theme_data/mirai_elevated_button_theme_data.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:mirai/mirai.dart';
-import 'package:mirai/src/utils/color_utils.dart';
 
 part 'mirai_elevated_button_theme_data.freezed.dart';
 part 'mirai_elevated_button_theme_data.g.dart';
@@ -17,24 +16,6 @@ class MiraiElevatedButtonThemeData with _$MiraiElevatedButtonThemeData {
 
 extension MiraiThemeParser on MiraiElevatedButtonThemeData {
   ElevatedButtonThemeData get parse {
-    return ElevatedButtonThemeData(style: _style(style));
-  }
-
-  ButtonStyle _style(MiraiButtonStyle? style) {
-    return ElevatedButton.styleFrom(
-      foregroundColor: style?.foregroundColor.toColor,
-      backgroundColor: style?.backgroundColor.toColor,
-      disabledForegroundColor: style?.disabledForegroundColor.toColor,
-      disabledBackgroundColor: style?.disabledBackgroundColor.toColor,
-      shadowColor: style?.shadowColor.toColor,
-      surfaceTintColor: style?.surfaceTintColor.toColor,
-      elevation: style?.elevation,
-      textStyle: style?.textStyle?.parse,
-      enableFeedback: style?.enableFeedback,
-      minimumSize: style?.minimumSize?.parse,
-      fixedSize: style?.fixedSize?.parse,
-      maximumSize: style?.maximumSize?.parse,
-      padding: style?.padding.parse,
-    );
+    return ElevatedButtonThemeData(style: style?.parseElevated);
   }
 }
