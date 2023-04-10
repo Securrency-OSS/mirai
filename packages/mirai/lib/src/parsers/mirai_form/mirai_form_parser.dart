f02168f09013c1b3e6fee1f8d814c990977e4116
Aasim Khan <iamasimkhanjadoon@gmail.com>
2023-04-10 11:36:30 +0400

    Added basic structure for Form widget.

---
+++ packages/mirai/lib/src/parsers/mirai_form/mirai_form_parser.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mirai/src/action/mirai_action_parser.dart';
+import 'package:mirai/src/framework/framework.dart';
+import 'package:mirai/src/parsers/mirai_form/mirai_form.dart';
+import 'package:mirai/src/utils/widget_type.dart';
+
+class MiraiFormParser extends MiraiParser<MiraiForm> {
+  const MiraiFormParser();
+
+  @override
+  MiraiForm getModel(Map<String, dynamic> json) => MiraiForm.fromJson(json);
+
+  @override
+  String get type => WidgetType.floatingActionButton.name;
+
+  @override
+  Widget parse(BuildContext context, MiraiForm model) {
+    return Form(
+      onChanged: model.onChanged == null
+          ? null
+          : () => model.onChanged.onCall(context),
+      autovalidateMode: model.autovalidateMode,
+      child: Mirai.fromJson(model.child, context) ?? const SizedBox(),
+    );
+  }
+}
