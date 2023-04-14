ad04eeba0a3d9785f6ab169fdae4b46a05bad48a
Aasim Khan <iamasimkhanjadoon@gmail.com>
2023-04-14 15:52:17 +0400

    refactor: Input validation types are renamed, and static regex expressions are moved inside the enum

--- packages/mirai/lib/src/utils/input_validations.dart
+++ packages/mirai/lib/src/utils/input_validations.dart
@@ -1,29 +1,29 @@
-RegExp emailRegExp = RegExp(
-    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
-RegExp nameRegExp = RegExp(r"^[A-Za-z .`'/-]{2,32}$");
-RegExp passwordRegExp = RegExp(
-    r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$%^&£*\-_+=[\]{}|\\:,?\/`~""()<>;!]*$');
-RegExp notEmptyRegExp = RegExp("^[0-9a-zA-Z .`'/-]{1,50}\$");
-
 enum InputValidationType {
-  validateEmail,
-  validateName,
-  validatePassword,
-  notEmpty;
+  isEmail,
+  isName,
+  isPassword,
+  isNotEmpty;
+
+  RegExp get _emailRegExp => RegExp(
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+  RegExp get _nameRegExp => RegExp(r"^[A-Za-z .`'/-]{2,32}$");
+  RegExp get _passwordRegExp => RegExp(
+      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$%^&£*\-_+=[\]{}|\\:,?\/`~""()<>;!]*$');
+  RegExp get _isNotEmptyRegExp => RegExp("^[0-9a-zA-Z .`'/-]{1,50}\$");
 
   bool validate(String value, String rule) {
     switch (this) {
-      case InputValidationType.validateEmail:
-        return emailRegExp.hasMatch(value);
+      case InputValidationType.isEmail:
+        return _emailRegExp.hasMatch(value);
 
-      case InputValidationType.validateName:
-        return nameRegExp.hasMatch(value);
+      case InputValidationType.isName:
+        return _nameRegExp.hasMatch(value);
 
-      case InputValidationType.validatePassword:
-        return passwordRegExp.hasMatch(value);
+      case InputValidationType.isPassword:
+        return _passwordRegExp.hasMatch(value);
 
-      case InputValidationType.notEmpty:
-        return notEmptyRegExp.hasMatch(value);
+      case InputValidationType.isNotEmpty:
+        return _isNotEmptyRegExp.hasMatch(value);
 
       default:
         return RegExp(rule).hasMatch(value);
