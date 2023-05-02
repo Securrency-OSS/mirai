7cd85b12ecae610d1276cfc0a2060534abd9b693
Aasim Khan <iamasimkhanjadoon@gmail.com>
2023-05-02 11:56:44 +0400

    feat: Network-wrapper can handle JSON and json-from-assets-path

--- packages/mirai/lib/src/parsers/mirai_network_wrapper/mirai_network_wrapper_parser.dart
+++ packages/mirai/lib/src/parsers/mirai_network_wrapper/mirai_network_wrapper_parser.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mirai/mirai.dart';
 import 'package:mirai/src/parsers/mirai_network_wrapper/mirai_network_wrapper.dart';
 import 'package:mirai/src/utils/log.dart';
@@ -18,20 +21,44 @@ class MiraiNetworkWrapperParser extends MiraiParser<MiraiNetworkWrapper> {
   Widget parse(BuildContext context, MiraiNetworkWrapper model) {
     Map<String, dynamic> bodyJson = Map<String, dynamic>.from(model.body);
 
-    model.data.forEach((key, value) {
-      readItAll(bodyJson, key, value);
-    });
+    if (bodyJson.containsKey("assetPath")) {
+      return FutureBuilder(
+        future: _loadFromAssets(bodyJson["assetPath"]),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            bodyJson = snapshot.data;
+            model.data.forEach((key, value) {
+              _updateBodyJson(bodyJson, key, value);
+            });
 
-    return Mirai.fromJson(bodyJson, context) ?? const SizedBox();
+            return Mirai.fromJson(bodyJson, context) ?? const SizedBox();
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      );
+    } else {
+      model.data.forEach((key, value) {
+        _updateBodyJson(bodyJson, key, value);
+      });
+      return Mirai.fromJson(bodyJson, context) ?? const SizedBox();
+    }
   }
 
-  readItAll(Map<String, dynamic> data, String key, dynamic value) {
+  Future<Map<String, dynamic>> _loadFromAssets(String assetPath) async {
+    final String data = await rootBundle.loadString(assetPath);
+    return jsonDecode(data);
+  }
+
+  _updateBodyJson(Map<String, dynamic> data, String key, dynamic value) {
     for (MapEntry<String, dynamic> mapEntry in data.entries) {
       if (mapEntry.value == key) {
         try {
           data.update(
             mapEntry.key,
-            (existingValue) => value,
+            (existingValue) => "$value",
           );
         } catch (e) {
           Log.e(e);
@@ -41,12 +68,12 @@ class MiraiNetworkWrapperParser extends MiraiParser<MiraiNetworkWrapper> {
       }
 
       if (mapEntry.value is Map) {
-        readItAll(mapEntry.value, key, value);
+        _updateBodyJson(mapEntry.value, key, value);
       }
 
       if (mapEntry.value is List) {
         for (Map<String, dynamic> listItem in mapEntry.value) {
-          readItAll(listItem, key, value);
+          _updateBodyJson(listItem, key, value);
         }
       }
     }
