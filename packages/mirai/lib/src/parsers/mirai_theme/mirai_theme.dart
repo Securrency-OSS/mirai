36c717b8ac8e793410e884c2c1ac72baf5c6f548
Aasim Khan <iamasimkhanjadoon@gmail.com>
2023-05-26 16:35:31 +0400

    Added `DialogTheme` in Mirai Theme data

--- packages/mirai/lib/src/parsers/mirai_theme/mirai_theme.dart
+++ packages/mirai/lib/src/parsers/mirai_theme/mirai_theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:mirai/src/parsers/mirai_button_style/mirai_button_style.dart';
 import 'package:mirai/src/parsers/mirai_color_scheme/mirai_color_scheme.dart';
+import 'package:mirai/src/parsers/mirai_dialog_theme/mirai_dialog_theme.dart';
 import 'package:mirai/src/parsers/mirai_icon_theme_data/mirai_icon_theme_data.dart';
 import 'package:mirai/src/parsers/mirai_material_color/mirai_material_color.dart';
 import 'package:mirai/src/utils/color_utils.dart';
@@ -41,6 +42,7 @@ class MiraiTheme with _$MiraiTheme {
     MiraiButtonStyle? iconButtonTheme,
     MiraiIconThemeData? iconTheme,
     MiraiIconThemeData? primaryIconTheme,
+    MiraiDialogTheme? dialogTheme,
   }) = _MiraiTheme;
 
   factory MiraiTheme.fromJson(Map<String, dynamic> json) =>
@@ -80,6 +82,7 @@ extension MiraiThemeParser on MiraiTheme {
       iconButtonTheme: IconButtonThemeData(style: iconButtonTheme?.parseIcon),
       iconTheme: iconTheme?.parse,
       primaryIconTheme: primaryIconTheme?.parse,
+      dialogTheme: dialogTheme?.parse,
     );
   }
 }
--- packages/mirai/lib/src/parsers/mirai_theme/mirai_theme.g.dart
+++ packages/mirai/lib/src/parsers/mirai_theme/mirai_theme.g.dart
@@ -58,6 +58,10 @@ _$_MiraiTheme _$$_MiraiThemeFromJson(Map<String, dynamic> json) =>
           ? null
           : MiraiIconThemeData.fromJson(
               json['primaryIconTheme'] as Map<String, dynamic>),
+      dialogTheme: json['dialogTheme'] == null
+          ? null
+          : MiraiDialogTheme.fromJson(
+              json['dialogTheme'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$$_MiraiThemeToJson(_$_MiraiTheme instance) =>
@@ -91,6 +95,7 @@ Map<String, dynamic> _$$_MiraiThemeToJson(_$_MiraiTheme instance) =>
       'iconButtonTheme': instance.iconButtonTheme,
       'iconTheme': instance.iconTheme,
       'primaryIconTheme': instance.primaryIconTheme,
+      'dialogTheme': instance.dialogTheme,
     };
 
 const _$BrightnessEnumMap = {
