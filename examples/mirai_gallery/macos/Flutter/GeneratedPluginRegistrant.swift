ef11ae43adc8712bc2b196d1b071737d8d449933
Divyanshu Bhargava <divyanshu.bhargava@securrency.com>
2023-10-23 20:30:44 +0530

    Update pubspec

--- examples/mirai_gallery/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/mirai_gallery/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_database
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
 }
