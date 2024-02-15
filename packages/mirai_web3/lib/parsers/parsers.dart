3d71248dabc4e20b8f1ed95a2f1c062524a86740
Aasim Khan <iamasimkhanjadoon@gmail.com>
2024-02-15 18:58:40 +0400

    refactor: Removed web3 connect button and sign button

--- packages/mirai_web3/lib/parsers/parsers.dart
+++ packages/mirai_web3/lib/parsers/parsers.dart
@@ -1,4 +1,2 @@
 export 'package:mirai_web3/parsers/web3_account_button/web3_account_button.dart';
-export 'package:mirai_web3/parsers/web3_connect_button/web3_connect_button.dart';
-export 'package:mirai_web3/parsers/web3_sign_button/web3_sign_button.dart';
 export 'package:mirai_web3/parsers/web3_signature_text/web3_signature_text.dart';
