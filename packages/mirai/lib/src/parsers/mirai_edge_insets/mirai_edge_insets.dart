ef87b981683f0ba5c8a3c2aa60f318838365149f
superiorsd10 <sachindapkara6@gmail.com>
2024-02-29 14:56:32 +0530

    feat(mirai_edge_insets): customize the fromJson factory to support num and List as input

--- packages/mirai/lib/src/parsers/mirai_edge_insets/mirai_edge_insets.dart
+++ packages/mirai/lib/src/parsers/mirai_edge_insets/mirai_edge_insets.dart
@@ -13,8 +13,33 @@ class MiraiEdgeInsets with _$MiraiEdgeInsets {
     double? bottom,
   }) = _MiraiEdgeInsets;
 
-  factory MiraiEdgeInsets.fromJson(Map<String, dynamic> json) =>
-      _$MiraiEdgeInsetsFromJson(json);
+  factory MiraiEdgeInsets.fromJson(dynamic json) => _fromJson(json);
+
+  static MiraiEdgeInsets _fromJson(dynamic json) {
+    Map<String, dynamic> resultantJson;
+
+    if (json is num) {
+      resultantJson = {
+        "left": json,
+        "top": json,
+        "right": json,
+        "bottom": json
+      };
+    } else if (json is List<num> && json.length == 4) {
+      resultantJson = {
+        "left": json[0],
+        "top": json[1],
+        "right": json[2],
+        "bottom": json[3]
+      };
+    } else if (json is Map<String, dynamic>) {
+      resultantJson = json;
+    } else {
+      throw ArgumentError('Invalid input format for MiraiEdgeInsets');
+    }
+
+    return _$MiraiEdgeInsetsFromJson(resultantJson);
+  }
 }
 
 extension MiraiEdgeInsetsParser on MiraiEdgeInsets? {
