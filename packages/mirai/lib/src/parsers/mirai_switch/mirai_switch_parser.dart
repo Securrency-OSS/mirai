a7c158c1aec1eabba8418ceda4dc9d787fb498e0
Divyanshu Bhargava <divyanshub024@gmail.com>
2024-03-02 12:53:59 +0400

    Refactor Mirai Switch Widget

--- packages/mirai/lib/src/parsers/mirai_switch/mirai_switch_parser.dart
+++ packages/mirai/lib/src/parsers/mirai_switch/mirai_switch_parser.dart
@@ -1,20 +1,162 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mirai/src/parsers/mirai_switch/mirai_switch.dart';
-import 'package:mirai/src/parsers/mirai_switch/widget/mirai_switch_widget.dart';
-import 'package:mirai/src/utils/widget_type.dart';
-import 'package:mirai_framework/mirai_framework.dart';
+import 'package:mirai/mirai.dart';
+import 'package:mirai/src/parsers/mirai_material_color/mirai_material_color.dart';
+import 'package:mirai/src/utils/action_type.dart';
 
 class MiraiSwitchParser extends MiraiParser<MiraiSwitch> {
   const MiraiSwitchParser();
+
+  /// The reason this type does not come from [ActionType] is because any value cannot be named as "switch".
+  @override
+  String get type => "switch";
 
   @override
   MiraiSwitch getModel(Map<String, dynamic> json) => MiraiSwitch.fromJson(json);
 
   @override
-  String get type => WidgetType.switchButton.name;
+  Widget parse(BuildContext context, MiraiSwitch model) {
+    return _SwitchWidget(model: model);
+  }
+}
+
+class _SwitchWidget extends StatefulWidget {
+  const _SwitchWidget({required this.model});
+
+  final MiraiSwitch model;
 
   @override
-  Widget parse(BuildContext context, MiraiSwitch model) {
-    return MiraiSwitchWidget(model: model);
+  State<_SwitchWidget> createState() => _SwitchWidgetState();
+}
+
+class _SwitchWidgetState extends State<_SwitchWidget> {
+  bool isSelected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isSelected = widget.model.value ?? false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final MiraiSwitch model = widget.model;
+
+    switch (model.switchType) {
+      case MiraiSwitchType.cupertino:
+        return _buildCupertinoSwitch(context, model);
+      case MiraiSwitchType.adaptive:
+        return _buildAdaptiveSwitch(context, model);
+      case MiraiSwitchType.material:
+      default:
+        return _buildMaterialSwitch(context, model);
+    }
+  }
+
+  /// Change the value of the switch as the user toggles it.
+  void _onChanged(bool value) {
+    isSelected = value;
+    if (widget.model.onChanged != null) {
+      Mirai.onCallFromJson(widget.model.onChanged, context);
+    }
+    setState(() {});
+  }
+
+  Icon? thumbIconWidget(BuildContext context, Map<String, dynamic>? thumbIcon) {
+    if (thumbIcon == null) return null;
+
+    final Widget? widget = Mirai.fromJson(thumbIcon, context);
+    if (widget != null && widget is Icon) {
+      return widget;
+    }
+
+    return null;
+  }
+
+  Widget _buildCupertinoSwitch(BuildContext context, MiraiSwitch model) {
+    return CupertinoSwitch(
+      value: isSelected,
+      onChanged: _onChanged,
+      activeColor: model.activeColor.toColor(context),
+      trackColor: model.trackColor?.parse(context),
+      thumbColor: model.thumbColor?.parse(context),
+      applyTheme: model.applyTheme,
+      focusColor: model.focusColor?.toColor(context),
+      onLabelColor: model.onLabelColor?.toColor(context),
+      offLabelColor: model.offLabelColor?.toColor(context),
+      autofocus: model.autofocus,
+      dragStartBehavior: model.dragStartBehavior,
+    );
+  }
+
+  Widget _buildAdaptiveSwitch(BuildContext context, MiraiSwitch model) {
+    return Switch.adaptive(
+      value: isSelected,
+      onChanged: _onChanged,
+      activeColor: model.activeColor?.toColor(context),
+      activeTrackColor: model.activeTrackColor?.toColor(context),
+      inactiveThumbColor: model.inactiveThumbColor?.toColor(context),
+      inactiveTrackColor: model.inactiveTrackColor?.toColor(context),
+      activeThumbImage: model.activeThumbImage != null
+          ? NetworkImage(model.activeThumbImage!)
+          : null,
+      inactiveThumbImage: model.inactiveThumbImage != null
+          ? NetworkImage(model.inactiveThumbImage!)
+          : null,
+      materialTapTargetSize: model.materialTapTargetSize,
+      thumbColor: MaterialStateProperty.all(model.thumbColor?.parse(context)),
+      trackColor: MaterialStateProperty.all(model.trackColor?.parse(context)),
+      trackOutlineColor: MaterialStateProperty.all(
+        model.trackOutlineColor?.parse(context),
+      ),
+      trackOutlineWidth: MaterialStateProperty.all(model.trackOutlineWidth),
+      thumbIcon: MaterialStateProperty.all(
+        Mirai.fromJson(model.thumbIcon, context) as Icon?,
+      ),
+      dragStartBehavior: model.dragStartBehavior,
+      focusColor: model.focusColor?.toColor(context),
+      hoverColor: model.hoverColor?.toColor(context),
+      overlayColor: MaterialStateProperty.all(
+        model.overlayColor?.parse(context),
+      ),
+      splashRadius: model.splashRadius,
+      autofocus: model.autofocus,
+      applyCupertinoTheme: model.applyCupertinoTheme,
+    );
+  }
+
+  Widget _buildMaterialSwitch(BuildContext context, MiraiSwitch model) {
+    return Switch(
+      value: isSelected,
+      onChanged: _onChanged,
+      activeColor: model.activeColor.toColor(context),
+      activeTrackColor: model.activeTrackColor.toColor(context),
+      inactiveThumbColor: model.inactiveThumbColor.toColor(context),
+      inactiveTrackColor: model.inactiveTrackColor.toColor(context),
+      activeThumbImage: model.activeThumbImage != null
+          ? NetworkImage(model.activeThumbImage!)
+          : null,
+      inactiveThumbImage: model.inactiveThumbImage != null
+          ? NetworkImage(model.inactiveThumbImage!)
+          : null,
+      thumbColor: MaterialStateProperty.all(model.thumbColor?.parse(context)),
+      trackColor: MaterialStateProperty.all(model.trackColor?.parse(context)),
+      trackOutlineColor: MaterialStateProperty.all(
+        model.trackOutlineColor?.parse(context),
+      ),
+      trackOutlineWidth: MaterialStateProperty.all(model.trackOutlineWidth),
+      thumbIcon: MaterialStateProperty.all(
+        Mirai.fromJson(model.thumbIcon, context) as Icon?,
+      ),
+      materialTapTargetSize: model.materialTapTargetSize,
+      dragStartBehavior: model.dragStartBehavior,
+      focusColor: model.focusColor?.toColor(context),
+      hoverColor: model.hoverColor?.toColor(context),
+      overlayColor: MaterialStateProperty.all(
+        model.overlayColor?.parse(context),
+      ),
+      splashRadius: model.splashRadius,
+      autofocus: model.autofocus,
+    );
   }
 }
--- packages/mirai/lib/src/utils/widget_type.dart
+++ packages/mirai/lib/src/utils/widget_type.dart
@@ -36,7 +36,6 @@ enum WidgetType {
   expanded,
   flexible,
   safeArea,
-  switchButton,
   align,
   pageView,
   refreshIndicator,
